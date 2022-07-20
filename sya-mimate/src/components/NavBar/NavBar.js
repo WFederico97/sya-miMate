44042f8ded5f6568063f1b3d7c2339d12491a17c
Federico W <wuthrich09@gmail.com>
2022-07-20 01:18:25 -0300

    FIX: Navbar responsive y agregado de form buscador

--- sya-mimate/src/components/NavBar/NavBar.js
+++ sya-mimate/src/components/NavBar/NavBar.js
@@ -2,13 +2,16 @@ import logo from  '../Images/logo-mimate.png'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './NavBar.css'
+
 
 
 function NavBarLogo() {
     return (
       <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
         <Container className='d-flex' >
           <Navbar.Brand href="#home" className='brand m-2'>
             <img
@@ -22,13 +25,22 @@ function NavBarLogo() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav justify variant='tabs' className="m-auto navLinks">
+            <Nav className="m-auto navLinks">
               <Nav.Link href="#Mates">Mates</Nav.Link>
               <Nav.Link href="#Bombillas">Bombillas</Nav.Link>
               <Nav.Link href="#Yerbas">Yerbas</Nav.Link>
               <Nav.Link href="#Termos">Termos</Nav.Link>
               <Nav.Link href="#Accesorios">Accesorios</Nav.Link>
             </Nav>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar producto/s"
+              className="me-2"
+              aria-label="Buscar producto/s"
+            />
+            <Button variant="outline-light">Buscar</Button>
+          </Form>
             </Navbar.Collapse>
         </Container>
       </Navbar>
