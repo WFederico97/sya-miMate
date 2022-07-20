3753e5fe2c8cf69dfab1338aa7112ab33328c97d
Federico W <wuthrich09@gmail.com>
2022-07-19 23:59:40 -0300

    REACT: agregado logo y modificacion de maquetado

---
+++ sya-mimate/src/components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import logo from  '../Images/logo-mimate.png'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './NavBar.css'
+
+
+function NavBarLogo() {
+    return (
+      <>
+      <Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
+        <Container className='d-flex' >
+          <Navbar.Brand href="#home" className='brand m-2'>
+            <img
+              alt=""
+              src={logo}
+              className=" align-center img-fluid logoMate"
+            />{' '}
+              <hr></hr>
+              <p className='text'> SyA Mi Mate</p>
+             
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav justify variant='tabs' className="m-auto navLinks">
+              <Nav.Link href="#Mates">Mates</Nav.Link>
+              <Nav.Link href="#Bombillas">Bombillas</Nav.Link>
+              <Nav.Link href="#Yerbas">Yerbas</Nav.Link>
+              <Nav.Link href="#Termos">Termos</Nav.Link>
+              <Nav.Link href="#Accesorios">Accesorios</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+    );
+  }
+  
+  export default NavBarLogo;
