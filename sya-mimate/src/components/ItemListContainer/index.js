77d90a6e5eff60ebad3cb513a67713825a9c47a0
Federico W <wuthrich09@gmail.com>
2022-07-26 00:59:17 -0300

    FIX: cambio de h2 por cards para catalogo de productos

--- sya-mimate/src/components/ItemListContainer/index.js
+++ sya-mimate/src/components/ItemListContainer/index.js
@@ -1,9 +1,11 @@
 import './index.css'
+import CardProducto from '../Cards/Cards';
 
 export default function ItemListContainer () {
     return (
-        <h5 className='textoCatalogo fs-3 text-decoration-underline fw-semibold bg-success text-wrap '>Somos un emprendimiento joven dedicado a la venta de productos para el mate, buscando ofrecer gran variabilidad y accesibilidad para todos los gustos. 
-            Fieles convencidos de que la felicidad está en el contacto humano y qué mejor que unos buenos mates para compartir momentos !
-        </h5>
+        <div className='producto flex-container row justify-content-center wrap'>
+            <CardProducto/>
+            <CardProducto/>
+        </div>
     );
 }
