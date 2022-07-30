cdb33ca6ced6ea2d657573fe6582ff71fa5df2bd
Federico W <wuthrich09@gmail.com>
2022-07-30 01:24:10 -0300

    ADD: item list

---
+++ sya-mimate/src/components/ItemList/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Item from '../Item';
+export default function ItemList() {
+  return (
+    <div className='productos container'>
+            <Item/>
+    </div>
+  )
+}
